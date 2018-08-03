36918f607a5d83510c5050ca329a501dc4c71d68
min kim <minwookkim2013@u.northwestern.edu>
2018-08-03 07:07:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,24 @@ function nowServing(entry){
 }
 
 function currentLine(array){
+<<<<<<< HEAD
   if(array.length === 0){
     var msg ="The line is currently empty.";
     return msg
+=======
+  if(array === 0){
+    var msg ="The line is currently empty."
+    console.log(msg)
+>>>>>>> c788a747a0cad31084611d51549ed154c865a334
   }else{
     for (let i = 0; i<array.length; i++){
       array[i] = " "+(i+1)+". "+array[i];
     }
     var stored3 = "The line is currently:"+array
+<<<<<<< HEAD
     return stored3
+=======
+    console.log(stored3)
+>>>>>>> c788a747a0cad31084611d51549ed154c865a334
   }
 }
